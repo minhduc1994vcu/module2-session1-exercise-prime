a728f39b8e5aa2b510c6f156dd15395b8a319ea6
minhduc <minhduc1994vcu@gmail.com>
2019-03-27 10:08:05 +0700

    finished project

--- src/main/java/Prime.java
+++ src/main/java/Prime.java
@@ -1,2 +1,29 @@
+import java.util.Scanner;
+
 public class Prime {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("enter a number which you want to check: ");
+        int number = scanner.nextInt();
+        if (number < 2) {
+            System.out.printf("%d is not a prime", number);
+        } else {
+            int i = 2;
+            boolean isprime = true;
+            while (i <= Math.sqrt(number)) {
+                if (number % i == 0) {
+                    isprime = false;
+                }
+                i++;
+            }
+            if (isprime) {
+                System.out.println(number + " is a prime");
+            } else {
+                System.out.println(number + " is not a prime");
+            }
+
+
+        }
+    }
+
 }
